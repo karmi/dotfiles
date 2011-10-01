3d9e623dca558b36cde824f05e25e685f22cf54a
Karel Minarik <karmi@karmi.cz>
2011-10-01 17:54:32 +0200

    [MAC] Added the Meslo typeface [https://github.com/andreberg/Meslo-Font]

--- mac/customize.sh
+++ mac/customize.sh
@@ -10,6 +10,13 @@ set_system_preferences
 echo "  → Customizing applications..."
 set_application_preferences
 
+if [ ! -d "$HOME/Fonts/Meslo LG DZ v1.0" ]; then
+  echo "  → Installing the Meslo typeface..."
+  curl -# -o /tmp/Meslo.zip -L -k http://github.com/downloads/andreberg/Meslo-Font/Meslo%20LG%20DZ%20v1.0.zip
+  open -W /tmp/Meslo.zip
+  cp -r "/tmp/Meslo LG DZ v1.0" $HOME/Fonts
+fi
+
 echo "  → Installing Terminal theme..."
 open $HOME/dotfiles/mac/terminal/karmi.terminal
 sleep 1
